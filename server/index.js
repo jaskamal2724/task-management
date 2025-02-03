5d334d9a629d679ef7324c5f78d360a23da25c1d
Nakul61102 <nakul358panchal@gmail.com>
2025-02-03 23:52:48 +0530

    Made connection for atlas made folder for middleware renamed db.js to connect.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-import connectDB from "./db/db.js";
+import connectDB from "./db/connect.js";
 import app from "./app.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config({
     path:"./.env"
 })
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000;
 
 connectDB()
 .then(()=>{
     app.listen(PORT,()=>{
-        console.log("server running on port :" , PORT)
+        console.log("server running on port :" , PORT);
     })
 })
 .catch((error)=>{
-    console.log(error)
+    console.log(error);
 })
 
